2bcd606b554627dc9856d1c3219dc05d0d389f94
Thomas Krampe <thomas.krampe@myctx.net>
2022-04-29 13:00:05 +0200

    Prepare Desktop

--- powershell/ras_startup.ps1
+++ powershell/ras_startup.ps1
@@ -6,6 +6,10 @@ if (!(Test-Path "C:\Install")) {
     New-Item -itemType Directory -Path C:\ -Name Install
     }
 
+if (!(Test-Path "C:\Temp")) {
+        New-Item -itemType Directory -Path C:\ -Name Temp
+        }
+        
 # Install Evergreen and applications
 # Invoke-WebRequest "https://raw.githubusercontent.com/Deyda/Evergreen-Script/main/Evergreen.ps1" -OutFile "C:\Install\Evergreen.ps1" 
 # Invoke-WebRequest "https://raw.githubusercontent.com/T13KDEV/Scripts/devel/config/LastSetting.txt" -OutFile "C:\Install\LastSetting.txt"
@@ -31,6 +35,25 @@ Start-Sleep -Seconds 30
 
 & dism /online /Export-DefaultAppAssociations:%TMP%\AppAssoc.xml  
 & dism /online /Import-DefaultAppAssociations:%TMP%\AppAssoc.xml
+
+# Prepare User Desktop
+Remove-Item 'C:\Users\Public\Desktop\Command Prompt.lnk' -Force
+Remove-Item 'C:\Users\Public\Desktop\Disk Management.lnk' -Force
+Remove-Item 'C:\Users\Public\Desktop\Google Chrome.lnk' -Force
+Remove-Item 'C:\Users\Public\Desktop\Internet Explorer.lnk' -Force
+Remove-Item 'C:\Users\Public\Desktop\Microsoft Edge.lnk' -Force
+
+New-PSDrive -Name "Sources" -PSProvider "FileSystem" -Root "\\lic-wfs-1\Sources"
+
+Copy-Item -Path Sources:\Abmelden\Abmelden.cmd -Destination C:\Temp
+Copy-Item -Path Sources:\Abmelden\Abmelden.ico -Destination C:\Temp
+Copy-Item -Path Sources:\Abmelden\Abmelden.lnk -Destination C:\Users\Public\Desktop
+Copy-Item -Path Sources:\LayoutModification.xml -Destination C:\Users\Default\AppData\Local\Microsoft\Windows\Shell
+
+Remove-PSDrive -Name "Sources"
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\Shell\Bags\1\Desktop" -Name "FFlags" -Value 1075839525
+stop-process -name explorer –force
 
 # Join Machine to Domain and restart
 Start-Sleep -s 30
